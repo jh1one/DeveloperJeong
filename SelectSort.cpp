2cb08ead6b1e1138a66881ee3bf6456f6ca9b478
Jeong <jh1one@naver.com>
2018-12-12 00:03:52 +0900

    Add SelectSort.cpp

---
+++ SelectSort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void SelectSort(int* arr, int size)
+{
+	int i, j, min, temp, idx;
+	for (i = 0; i < size; i++) {
+		min = 999999;
+		for (j = i; j < size; j++) {
+			if (min > arr[j]) {
+				min = arr[j];
+				idx = j;
+			}
+		}
+		temp = arr[idx];
+		arr[idx] = arr[i];
+		arr[i] = temp;
+	}
+}
+int main() {
+
+	int n;
+	cin >> n;
+	int *arr = new int[n];
+
+	for (int k = 0; k < n; k++)	cin >> arr[k];
+
+	SelectSort(arr, n);
+
+	for (int k = 0; k < n; k++)
+		cout << arr[k] << " ";
+
+	delete[] arr;
+	return 0;
+}
